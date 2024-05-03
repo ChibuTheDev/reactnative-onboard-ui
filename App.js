c3e3562b3480b9083c02ee106dda05f1d5c313a5
Chibu <chibueze.k.muoneke@gmail.com>
2024-05-03 23:58:57 +0100

    initial commit

--- App.js
+++ App.js
@@ -1,12 +1,88 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import colors from './assets/colors';
+import AppIntroSlider from 'react-native-app-intro-slider';
+import Onboard from './Onboard';
+import { SafeAreaView, StyleSheet, Text, Image, View} from 'react-native';
+import {
+  useFonts,
+  OpenSans_300Light,
+  OpenSans_400Regular,
+  OpenSans_500Medium,
+  OpenSans_600SemiBold,
+  OpenSans_700Bold,
+  OpenSans_800ExtraBold,
+} from '@expo-google-fonts/open-sans';
+
+
+const data = [
+  {
+    title: 'Save time by tracking your studies',
+    text: 'Schedule your classes, assignments, quizzes and more',
+    image: require('./assets/images/one.png'),
+    bg: '#59b2ab',
+  },
+  {
+    title: 'Stay on top of your education',
+    text: 'Reduce your stress, increase your productivity',
+    image: require('./assets/images/two.png'),
+    bg: '#febe29',
+  },
+  {
+    title: 'Spend more time doing the things you love',
+    text: "Get started within five minutes",
+    image: require('./assets/images/three.png'),
+    bg: '#22bcb5',
+  },
+];
+
+
+
+
+
+
+
 
 export default function App() {
+  let [fontsLoaded] = useFonts({
+    OpenSans_300Light,
+    OpenSans_400Regular,
+    OpenSans_500Medium,
+    OpenSans_600SemiBold,
+    OpenSans_700Bold,
+    OpenSans_800ExtraBold,
+  });
+   
+  if(!fontsLoaded){
+    return <Text>Loading...</Text>
+  }
+
+
+  function renderItem({ item }) {
+    return (
+      <View style={styles.slide}>
+        <Image source={item.image} style={styles.image} />
+        <View style={styles.textContainer}>
+          <Text style={styles.title}>{item.title}</Text>
+          <Text style={styles.text}>{item.text}</Text>
+        </View>
+      </View>
+    );
+  }
+  
+
+  function keyExtractor(item){
+    return(item.title);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+    <StatusBar translucent backgroundColor="transparent" />
+    <AppIntroSlider
+      keyExtractor={keyExtractor}
+      renderItem={renderItem}
+      data={data}
+    />
+  </SafeAreaView>
   );
 }
 
@@ -15,6 +91,37 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    justifyContent: colors.white,
+  },
+  slide: {
+    flex: 1,
+    alignItems: 'center',
     justifyContent: 'center',
+  },
+  image: {
+    marginVertical:60,
+  },
+  textContainer: {
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  title: {
+    fontSize: 24,
+    color: colors.black,
+    textAlign: 'center',
+    fontFamily: 'OpenSans_700Bold',
+    marginHorizontal: 20,
+  },
+  text: {
+    fontSize: 14,
+    color: colors.grey,
+    textAlign: 'center',
+    fontFamily: 'OpenSans_600SemiBold',
+    marginHorizontal: 20,
+    marginTop: 10,
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
   },
 });
---
+++ assets/colors.js
@@ -0,0 +1,8 @@
+const colors ={
+black: '#000000',
+grey: '#666666',
+blue: '#1C215D',
+white: '#FFFFFF',
+}
+
+export default colors
